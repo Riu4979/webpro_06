8057a4a24e0272b5cd04aebcbda5bbcb118d58c0
Riu <xx.daiya.hr999@gmail.com>
2024-12-03 10:51:16 +0900

    API追加

---
+++ public/test.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('#btn1');
+btn.addEventListener('click', () => {
+    console.log( "ボタンが押された" );
+});
+var plus2 = function( x, y ) {
+    return x + y;
+}
+
+console.log(  );
